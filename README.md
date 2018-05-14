ab52a98b2c858441e1296b01b8a63408eb41dd93
Chris Zielinski <ckz8780@gmail.com>
2018-05-14 19:31:10 +0000

    Round 5 - added buttons to call data object back to screen

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 A small demo of some AJAX testing. This is part of the Interactive Frontend Development Module for CodeInstitute.
 
+#### See commit history for details at each step!
+
 The index is located at https://ckz8780.github.io/ifd-ajax-test/index.html
--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-function getData(cb) {
+const baseURL = "https://swapi.co/api/";
+
+function getData(type, cb) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://swapi.co/api/");
-    xhr.send();
-    
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
     };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
 
-function printDataToConsole(data) {
-    console.log(data);
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
 }
-
-getData(printDataToConsole);
