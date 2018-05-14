073045e7deb6ccf00592d51972c7e2301e8b081f
Chris Zielinski <ckz8780@gmail.com>
2018-05-14 19:13:08 +0000

    Round 2 - added external setData function

--- main.js
+++ main.js
@@ -3,8 +3,17 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
 
+
+function setData(jsonData) {
+    var data = jsonData;
+    console.log(data);
+}
 xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+    if (this.readyState == 4 && this.status == 200) { // readyState == 4 --> Request is complete.
+        // document.getElementById("data").innerHTML = this.responseText;
+        // console.log(typeof(this.responseText)); // string
+        // console.log(typeof(JSON.parse(this.responseText))); // object
+        
+        setData(JSON.parse(this.responseText));
     }
 };
