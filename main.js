f3eee6d207f80320455b29ba6560873100d7668c
Chris Zielinski <ckz8780@gmail.com>
2018-05-14 19:17:55 +0000

    Round 3 - added timeout function to delay execution of console logging

--- main.js
+++ main.js
@@ -3,17 +3,13 @@ var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
 
-
-function setData(jsonData) {
-    var data = jsonData;
-    console.log(data);
-}
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) { // readyState == 4 --> Request is complete.
-        // document.getElementById("data").innerHTML = this.responseText;
-        // console.log(typeof(this.responseText)); // string
-        // console.log(typeof(JSON.parse(this.responseText))); // object
-        
-        setData(JSON.parse(this.responseText));
+        var data = JSON.parse(this.responseText);
     }
 };
+
+// Delay execution of console.log() until after onreadystatechange is completed (500ms)
+setTimeout(function() {
+    console.log(data);
+}, 500);
