682e88840c9af051aa8e1498b21da7885a42fff0
Chris Zielinski <ckz8780@gmail.com>
2018-05-14 19:24:11 +0000

    Round 4 - added callback function

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-var xhr = new XMLHttpRequest();
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://swapi.co/api/");
-xhr.send();
+    xhr.open("GET", "https://swapi.co/api/");
+    xhr.send();
+    
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) { // readyState == 4 --> Request is complete.
-        var data = JSON.parse(this.responseText);
-    }
-};
-
-// Delay execution of console.log() until after onreadystatechange is completed (500ms)
-setTimeout(function() {
+function printDataToConsole(data) {
     console.log(data);
-}, 500);
+}
+
+getData(printDataToConsole);
